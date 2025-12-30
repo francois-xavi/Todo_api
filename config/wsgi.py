5959b7d2de9ca20f1f76c0efad459d98e669c15b
François-Xavier Coudea <francois-xavier@intside.com>
2025-12-30 17:02:19 +0100

    feat: Add WSGI configuration with WhiteNoise for static file serving.

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
